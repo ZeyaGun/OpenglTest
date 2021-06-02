67c42d27b917d616124c24d1cdc7c7c02cc666e5
Zeya <50080904+ZeyaGun@users.noreply.github.com>
2021-06-03 00:03:04 +0900

    Coding : Projcection

--- PlaffyBird/PlaffyBird/main.cpp
+++ PlaffyBird/PlaffyBird/main.cpp
@@ -12,7 +12,7 @@
 const GLuint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel, uniformProjection;	
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -31,34 +31,52 @@ static const char* vShader = "													\n\
 																				\n\
 layout (location = 0) in vec3 pos;												\n\
 																				\n\
+out vec4 vCol;																	\n\
+\n\
 uniform mat4 model;															\n\
-																				\n\
+uniform mat4 projection;															\n\
+\n\
 void main()																		\n\
 {																				\n\
-	gl_Position = model * vec4(pos, 1.0);			\n\
+	gl_Position = projection * model * vec4(pos, 1.0);			\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);		\n\
 }";
 
 // Fragment Shader
 static const char* fShader = "									\n\
 #version 330													\n\
 																\n\
+in vec4 vCol;													\n\
+\n\
 out vec4 colour;												\n\
 																\n\
 void main()														\n\
 {																\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);							\n\
+	colour = vCol;												\n\
 }";
 
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+	0, 3, 1,
+	1, 3, 2,
+	2, 3, 0,
+	0, 1, 2
+	};
+
 	GLfloat vertices[] = {
-		-1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
+		-1.0f, -1.0f, 0.0f,	// 0.0, 0.0, 0.0 = Black
+		0.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 0.0f,	// 1.0, 0.0, 0.0 = Red
+		0.0f, 1.0f, 0.0f,	// 0.0, 1.0, 0.0 = Green
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -68,6 +86,7 @@ void CreateTriangle()
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
 }
@@ -135,6 +154,7 @@ void CompileShaders()
 	}
 
 	uniformModel = glGetUniformLocation(shader, "model");
+	uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 int main()
@@ -182,11 +202,15 @@ int main()
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
 	CreateTriangle();
 	CompileShaders();
+
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth/bufferHeight, 0.1f, 100.0f);
 
 	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow))
@@ -202,7 +226,7 @@ int main()
 		if (abs(triOffset) >= triMaxOffset)
 			direction = !direction;
 		
-		curAngle += 0.1f;
+		curAngle += 1.0f;
 		if (curAngle >= 360)
 			curAngle = 0.0f;
 
@@ -216,22 +240,22 @@ int main()
 
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
-		
-		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, -2.5f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 		glBindVertexArray(VAO);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glUseProgram(0);
